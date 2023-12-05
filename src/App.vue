ffe531da67fc6d2c673d48454fa02884eb2b6d64
Neonist0141 <SvetlovAnatolyS@gmail.com>
2023-12-05 21:31:03 +0300

    Сайт итогового проета. Создан header.

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<!-- src/App.vue -->
+
+<template>
+  <header>
+      <article>
+          <h1>Магазин рукодельных изделий</h1>
+      </article>
+      <Menu></Menu>
+  </header>
+</template>
+
+<script>
+import Menu from './components/Menu.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Menu
+  }
+};
+</script>
+
+<style>
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    grid-column: 1/3;
+    border-radius: 5px;
+    box-shadow: 0 0 5px dimgray;
+    padding: 0.5cm 1cm;
+  }
+</style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,42 @@
+<!-- Menu.vue -->
+
+<template>
+    <ul>
+      <li v-for="item in menuItems" :key="item.id">
+        {{ item.text }}
+      </li>
+    </ul>
+</template>
+
+<script>
+export default {
+  name: 'Menu-component',
+  data() {
+    return {
+      menuItems: [
+        { id: 1, text: 'Каталог' },
+        { id: 1, text: 'Аукцион' },
+        { id: 2, text: 'Контакты' },
+        { id: 3, text: 'О нас' }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+/* Стили, специфичные для этого компонента */
+ul {
+  list-style-type: none;
+  display: flex;
+  padding: 0;
+  gap: 15px;
+}
+
+li {
+  padding: 5px 15px; 
+  cursor: pointer;
+  background-color: #eee;
+  border-radius: 100px;
+}
+</style>
